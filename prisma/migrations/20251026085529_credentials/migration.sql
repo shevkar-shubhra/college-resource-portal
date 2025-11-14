802955a2a4d24e4f5298e5683f23a255ede60ee4
shevkar-shubhra <shubhrashevkar@gmail.com>
2025-11-14 22:08:56 +0530

    migration.sql modified

--- prisma/migrations/20251026085529_credentials/migration.sql
+++ prisma/migrations/20251026085529_credentials/migration.sql
@@ -1,9 +1,4 @@
-/*
-  Warnings:
 
-  - Added the required column `updatedAt` to the `User` table without a default value. This is not possible if the table is not empty.
-
-*/
 -- AlterTable
 ALTER TABLE "User" ADD COLUMN     "emailVerified" TIMESTAMP(3),
 ADD COLUMN     "image" TEXT,
